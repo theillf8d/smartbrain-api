aa7c4f7944e4c5e59a150a9fbaa889bc547869ae
theillf8d <bsdfool@gmail.com>
2019-07-06 21:44:08 -0400

    add knex.js, preliminary work in /register route.

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "michael",
+    password: "phr34k3r",
+    database: "smart-brain"
+  }
+});
 
 const app = express();
 
@@ -9,83 +20,89 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const database = {
-    users: [{
-        id: 123,
-        name: "John",
-        email: "john@gmail.com",
-        password: "cookies",
-        entries: 0,
-        joined: new Date()
+  users: [
+    {
+      id: 123,
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date()
     },
     {
-        id: 124,
-        name: "Sally",
-        email: "sally@yahoo.com",
-        password: "salad",
-        entries: 0,
-        joined: new Date()
+      id: 124,
+      name: "Sally",
+      email: "sally@yahoo.com",
+      password: "salad",
+      entries: 0,
+      joined: new Date()
     }
-    ]
-}
+  ]
+};
 
-app.get('/', (req, res) => {
-    res.send(database.users);
+app.get("/", (req, res) => {
+  res.send(database.users);
 });
 
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === Number(id)) {
-            found = true;
-            return res.json(user);
-        }
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === Number(id)) {
+      found = true;
+      return res.json(user);
+    }
+  });
+
+  if (!found) {
+    res.status(404).json("User not found");
+  }
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    // res.json('success');
+    res.json(database.users[0]);
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  const { name, email, password } = req.body;
+  db("users")
+    .returning("*")
+    .insert({
+      name: name,
+      email: email,
+      joined: new Date()
     })
-
-    if (!found) {
-        res.status(404).json('User not found');
-    }
-})
-
-app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-        // res.json('success');
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json('error logging in');
-    }
-});
-
-app.post('/register', (req, res) => {
-    const { name, email, password } = req.body;
-    database.users.push({
-        id: 125,
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date()
+    .then(user => {
+      res.json(user);
     });
-    res.send(database.users[database.users.length - 1]);
-})
+});
 
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    database.users.forEach(user => {
-        console.log(user.id, id);
-        if (user.id === Number(id)) {
-            found = true;
-            user.entries++
-            return res.json(user.entries);
-        }
-    })
-
-    if (!found) {
-        res.status(404).json('User not found');
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  database.users.forEach(user => {
+    console.log(user.id, id);
+    if (user.id === Number(id)) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
     }
-})
+  });
+
+  if (!found) {
+    res.status(404).json("User not found");
+  }
+});
 
 const port = 3000;
 app.listen(3000, () => {
-    console.log(`server listening on port ${port}`);
+  console.log(`server listening on port ${port}`);
 });
